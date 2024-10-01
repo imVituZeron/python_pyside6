8c1b59e274f2f993a8a1767d4f5d4ab283a329d1
Vitor De Paula Santos <vitorpaulasantos@gmail.com>
2024-10-01 17:02:00 -0300

    adding a main window and some buttons

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from PySide6.QtWidgets import QApplication, QPushButton
+from PySide6.QtWidgets import QApplication, QGridLayout, QPushButton, QWidget
 
 app = QApplication()
+
+# buttons
 button = QPushButton("texto to botao")
 button.setStyleSheet('font-size: 40px;')
-button.show()
 
-# button2 = QPushButton("textodobotao2)
-# button2.show()
+button2 = QPushButton("textodobotao2")
+button2.setStyleSheet('font-size: 40px;')
+
+button3 = QPushButton("textodobotao3")
+button3.setStyleSheet('font-size: 40px;')
+
+# layout
+layout = QGridLayout()
+layout.addWidget(button, 1, 1, 1, 1)
+layout.addWidget(button2, 1, 2, 1, 1)
+layout.addWidget(button3, 3, 1, 1, 2)
+
+# central
+central = QWidget()
+central.setLayout(layout)
+
+central.show()
 app.exec()
-
 
